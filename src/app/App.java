89c33decc063d67c08f8d753bf227c9fa6faeffc
karim <karimservin@outlook.es>
2024-09-24 20:11:57 -0300

    indicar los desarrolladores del proyecto al ejecutar el codigo

--- src/app/App.java
+++ src/app/App.java
@@ -11,7 +11,22 @@ public class App {
 		System.out.println("Bienvenido al sistema desarrollado para el TP de Ingeneria y Calidad de Software");
 	
 
+Alumno arreglo[];
 		
+		arreglo = new Alumno[3];
+		
+		arreglo[0] = new Alumno("Julian","Serrano",25429);
+		arreglo[1] = new Alumno("Julian","Serrana",25430);
+		arreglo[2] = new Alumno("Julian","Serra",25431);
+		
+		System.out.println("\n\n Desarrollado por: \n");
+		
+		for (int i = 0; i < 3; i++) {
+		    System.out.println(String.format("%s %s %d", 
+		        arreglo[i].getNombre(), 
+		        arreglo[i].getApellido(), 
+		        arreglo[i].getNumeroLegajo()));
+		}
 		
 	}
 	
